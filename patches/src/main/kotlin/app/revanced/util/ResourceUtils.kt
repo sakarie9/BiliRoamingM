5854b63f9f4c6bc48bb16c5dd7e5f8219fed9d9f
Maary <24504742+Steve-Mr@users.noreply.github.com>
2024-11-03 01:03:04 +0800

    Update ResourceUtils.kt

--- patches/src/main/kotlin/app/revanced/util/ResourceUtils.kt
+++ patches/src/main/kotlin/app/revanced/util/ResourceUtils.kt
@@ -1,54 +1,15 @@
 package app.revanced.util
 
 import app.revanced.patcher.data.ResourceContext
-import app.revanced.patcher.patch.PatchException
-import app.revanced.patcher.util.DomFileEditor
-import app.revanced.util.resource.BaseResource
-import org.w3c.dom.Attr
+import app.revanced.patcher.util.Document
 import org.w3c.dom.Element
 import org.w3c.dom.Node
 import org.w3c.dom.NodeList
-import java.io.InputStream
+import java.io.File
 import java.nio.file.Files
 import java.nio.file.StandardCopyOption
 
 private val classLoader = object {}.javaClass.classLoader
-
-/**
- * Returns a sequence for all child nodes.
- */
-fun NodeList.asSequence() = (0 until this.length).asSequence().map { this.item(it) }
-
-/**
- * Returns a sequence for all child nodes.
- */
-fun Node.childElementsSequence() = this.childNodes.asSequence().filter { it.nodeType == Node.ELEMENT_NODE }
-
-/**
- * Performs the given [action] on each child element.
- */
-fun Node.forEachChildElement(action: (Node) -> Unit) =
-    childElementsSequence().forEach {
-        action(it)
-    }
-
-/**
- * Recursively traverse the DOM tree starting from the given root node.
- *
- * @param action function that is called for every node in the tree.
- */
-fun Node.doRecursively(action: (Node) -> Unit) {
-    action(this)
-    for (i in 0 until this.childNodes.length) this.childNodes.item(i).doRecursively(action)
-}
-
-fun Node.insertFirst(node: Node) {
-    if (hasChildNodes()) {
-        insertBefore(node, firstChild)
-    } else {
-        appendChild(node)
-    }
-}
 
 /**
  * Copy resources from the current class loader to the resource directory.
@@ -56,28 +17,21 @@ fun Node.insertFirst(node: Node) {
  * @param sourceResourceDirectory The source resource directory name.
  * @param resources The resources to copy.
  */
-fun ResourceContext.copyResources(
-    sourceResourceDirectory: String,
-    vararg resources: ResourceGroup,
-) {
+fun ResourceContext.copyResources(sourceResourceDirectory: String, vararg resources: ResourceGroup) {
     val targetResourceDirectory = this["res", false]
 
     for (resourceGroup in resources) {
+        val resourceDirectory = resourceGroup.resourceDirectoryName
         resourceGroup.resources.forEach { resource ->
-            val resourceFile = "${resourceGroup.resourceDirectoryName}/$resource"
+            val resourceFile = "$resourceDirectory/$resource"
             Files.copy(
-                inputStreamFromBundledResource(sourceResourceDirectory, resourceFile)!!,
-                targetResourceDirectory.resolve(resourceFile).toPath(),
-                StandardCopyOption.REPLACE_EXISTING,
+                bundledResource("$sourceResourceDirectory/$resourceFile"),
+                File(targetResourceDirectory, resourceFile).toPath(),
+                StandardCopyOption.REPLACE_EXISTING
             )
         }
     }
 }
-
-internal fun inputStreamFromBundledResource(
-    sourceResourceDirectory: String,
-    resourceFile: String,
-): InputStream? = classLoader.getResourceAsStream("$sourceResourceDirectory/$resourceFile")
 
 /**
  * Resource names mapped to their corresponding resource data.
@@ -86,80 +40,70 @@ internal fun inputStreamFromBundledResource(
  */
 class ResourceGroup(val resourceDirectoryName: String, vararg val resources: String)
 
-/**
- * Iterate through the children of a node by its tag.
- * @param resource The xml resource.
- * @param targetTag The target xml node.
- * @param callback The callback to call when iterating over the nodes.
- */
-fun ResourceContext.iterateXmlNodeChildren(
-    resource: String,
-    targetTag: String,
-    callback: (node: Node) -> Unit,
-) = xmlEditor[classLoader.getResourceAsStream(resource)!!].use { editor ->
-    val document = editor.file
-
-    val stringsNode = document.getElementsByTagName(targetTag).item(0).childNodes
-    for (i in 1 until stringsNode.length - 1) callback(stringsNode.item(i))
+operator fun Document.get(tagName: String): Element {
+    return getElementsByTagName(tagName).item(0) as Element
 }
 
-// TODO: After the migration to the new patcher, remove the following code and replace it with the commented code below.
-fun String.copyXmlNode(source: DomFileEditor, target: DomFileEditor): AutoCloseable {
-    val hostNodes = source.file.getElementsByTagName(this).item(0).childNodes
+fun Node.children(): Sequence<Element> =
+    childNodes.iterator().asSequence().filterIsInstance<Element>()
 
-    val destinationResourceFile = target.file
-    val destinationNode = destinationResourceFile.getElementsByTagName(this).item(0)
-
-    for (index in 0 until hostNodes.length) {
-        val node = hostNodes.item(index).cloneNode(true)
-        destinationResourceFile.adoptNode(node)
-        destinationNode.appendChild(node)
-    }
-
-    return AutoCloseable {
-        source.close()
-        target.close()
+fun Node.walk(action: (Element) -> Unit) {
+    if (this is Element)
+        action(this)
+    children().forEach {
+        it.walk(action)
     }
 }
 
-// /**
-//  * Copies the specified node of the source [Document] to the target [Document].
-//  * @param source the source [Document].
-//  * @param target the target [Document]-
-//  * @return AutoCloseable that closes the [Document]s.
-//  */
-// fun String.copyXmlNode(
-//     source: Document,
-//     target: Document,
-// ): AutoCloseable {
-//     val hostNodes = source.getElementsByTagName(this).item(0).childNodes
-//
-//     val destinationNode = target.getElementsByTagName(this).item(0)
-//
-//     for (index in 0 until hostNodes.length) {
-//         val node = hostNodes.item(index).cloneNode(true)
-//         target.adoptNode(node)
-//         destinationNode.appendChild(node)
-//     }
-//
-//     return AutoCloseable {
-//         source.close()
-//         target.close()
-//     }
-// }
+var Node.tag: String
+    get() = nodeName
+    set(value) {
+        ownerDocument.renameNode(this, null, value)
+    }
 
-// @Deprecated(
-//     "Use copyXmlNode(Document, Document) instead.",
-//     ReplaceWith(
-//         "this.copyXmlNode(source.file as Document, target.file as Document)",
-//         "app.revanced.patcher.util.Document",
-//         "app.revanced.patcher.util.Document",
-//     ),
-// )
-// fun String.copyXmlNode(
-//     source: DomFileEditor,
-//     target: DomFileEditor,
-// ) = this.copyXmlNode(source.file as Document, target.file as Document)
+operator fun NodeList.iterator(): Iterator<Node> = object : Iterator<Node> {
+    private var index = 0
+    override fun hasNext(): Boolean = index < length
+    override fun next(): Node = item(index++)
+}
+
+operator fun Element.get(attrName: String): String = getAttribute(attrName)
+operator fun Element.set(attrName: String, attrValue: String): Unit = setAttribute(attrName, attrValue)
+
+fun Element.appendChild(tagName: String, build: Element.() -> Unit) {
+    appendChild(ownerDocument.createElement(tagName).apply(build))
+}
+
+fun Element.insertBefore(refChild: Node, tagName: String, build: Element.() -> Unit) {
+    insertBefore(ownerDocument.createElement(tagName).apply(build), refChild)
+}
+
+fun Element.insertChild(index: Int, tagName: String, build: Element.() -> Unit) {
+    insertBefore(ownerDocument.createElement(tagName).apply(build), children().elementAt(index))
+}
+
+fun Element.removeChild(condition: Element.() -> Boolean) {
+    children().find(condition)?.also { removeChild(it) }
+}
+
+fun bundledResource(path: String) = classLoader.getResourceAsStream(path)!!
+
+fun ResourceContext.mergeXmlNodes(
+    nodeName: String,
+    hostPath: String,
+    vararg paths: String
+) = document[hostPath].use { host ->
+    val hostNode = host[nodeName]
+    paths.map { document[bundledResource(it)] }.forEach { editor ->
+        editor.use {
+            for (node in it[nodeName].childNodes) {
+                val clonedNode = node.cloneNode(true)
+                host.adoptNode(clonedNode)
+                hostNode.appendChild(clonedNode)
+            }
+        }
+    }
+}
 
 /**
  * Add a resource node child.
@@ -199,13 +143,4 @@ internal fun NodeList.findElementByAttributeValue(attributeName: String, value: 
 
 internal fun NodeList.findElementByAttributeValueOrThrow(attributeName: String, value: String): Element {
     return findElementByAttributeValue(attributeName, value) ?: throw PatchException("Could not find: $attributeName $value")
-}
-
-internal fun Element.copyAttributesFrom(oldContainer: Element) {
-    // Copy attributes from the old element to the new element
-    val attributes = oldContainer.attributes
-    for (i in 0 until attributes.length) {
-        val attr = attributes.item(i) as Attr
-        setAttribute(attr.name, attr.value)
-    }
 }
